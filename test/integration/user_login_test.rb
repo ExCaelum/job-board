01feab922dfe9879bdfd6b097c88b3a35d1de286
Parker P <exxcaelum@gmail.com>
2016-06-04 18:06:47 -0600

    added and styled the ability to login

--- test/integration/user_login_test.rb
+++ test/integration/user_login_test.rb
@@ -2,12 +2,12 @@ require 'test_helper'
 
 class UserLoginTest < ActionDispatch::IntegrationTest
   test "registered user can login" do
-    user = User.create(username: "Parker", password: "password")
+    user = User.create(username: "Parker", password: "password", email_address: "sample@gmail.com", phone_number: "000-000-0000")
 
     visit login_path
 
-    fill_in "Username", with: user.username
-    fill_in "Password", with: user.password
+    fill_in "Username", with: "Parker"
+    fill_in "Password", with: "password"
     click_button "Login"
 
     assert page.has_content?("Parker's Homepage")
---
+++ test/integration/user_logout_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class UserLogoutTest < ActionDispatch::IntegrationTest
+  test "logged in user can logout" do
+    user = User.create(username: "Parker",
+                       password: "password",
+                       email_address: "sample@gmail.com",
+                       phone_number: "000-000-0000")
+
+    visit login_path
+    fill_in "Username", with: user.username
+    fill_in "Password", with: user.password
+    click_button "Login"
+    assert page.has_content?("Parker's Homepage")
+
+    click_link "Logout"
+
+    refute page.has_content?("Parker's Homepage")
+  end
+end
